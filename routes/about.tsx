4a3d1f21b0f343371104fba11d9d911e8415e4bd
Curious <zhanghaoqi89@163.com>
2022-08-29 15:52:51 +0800

    deno_test

---
+++ routes/about.tsx
@@ -0,0 +1,27 @@
+// 确保 jsx被正确渲染
+/** @jsx h */
+import { h } from "preact";
+import { Handlers } from "$fresh/server.ts";
+
+export const handler: Handlers = {
+  // async GET(req, ctx) {
+  //   const resp = await ctx.render(req);
+  //   resp.headers.set("X-Custom-Header", "Hello");
+  //   return resp;
+  // },
+  GET(req) {
+    const uuid = crypto.randomUUID();
+    return new Response(JSON.stringify(uuid), {
+      headers: { "Content-Type": "application/json" },
+    });
+  },
+};
+
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This is the about page.</p>
+    </main>
+  );
+}
---
+++ routes/countdown.tsx
@@ -0,0 +1,13 @@
+/** @jsx h */
+import { h } from "preact";
+import Countdown from "../islands/Countdown.tsx";
+
+export default function Page() {
+  const date = new Date();
+  date.setMinutes(date.getMinutes() + 1);
+  return (
+    <p>
+      The big event is happening <Countdown target={date.toISOString()} />.
+    </p>
+  );
+}
